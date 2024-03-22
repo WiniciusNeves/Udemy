7c1a636cd9d08f605eb9c5dab41b7102d59c588a
Winicius Neves <127152668+WiniciusNeves@users.noreply.github.com>
2024-03-22 19:25:49 -0300

    Create name.cs

---
+++ name.cs
@@ -0,0 +1,8 @@
+// Program.cs
+Console.WriteLine("Qual seu nome?");
+Console.WriteLine("Qual sua idade?");
+
+string name = Console.ReadLine();
+int idade = int.Parse(Console.ReadLine());
+Console.WriteLine($"{name} {idade}");
+
