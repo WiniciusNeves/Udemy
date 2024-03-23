5bde586c386566925619f8e484dd0c65bcf72f9c
Winicius Neves <127152668+WiniciusNeves@users.noreply.github.com>
2024-03-23 14:19:28 -0300

    Update Data.cs

--- Csharp/Data.cs
+++ Csharp/Data.cs
@@ -3,10 +3,3 @@ using System.Data;
 
 Console.WriteLine("Data e Hora: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
 
-// Program.cs
-Console.WriteLine("Qual seu nome?");
-Console.WriteLine("Qual sua idade?");
-
-string name = Console.ReadLine();
-int idade = int.Parse(Console.ReadLine());
-Console.WriteLine($"{name} {idade}");
