973b37212a7ceb5a51bbbfd2e14949c4e32eac0c
Winicius Neves <127152668+WiniciusNeves@users.noreply.github.com>
2024-03-23 14:17:27 -0300

    Delete name.cs

--- name.cs
+++
@@ -1,8 +0,0 @@
-// Program.cs
-Console.WriteLine("Qual seu nome?");
-Console.WriteLine("Qual sua idade?");
-
-string name = Console.ReadLine();
-int idade = int.Parse(Console.ReadLine());
-Console.WriteLine($"{name} {idade}");
-
